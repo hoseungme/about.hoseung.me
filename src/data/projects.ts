0ae1a82d1c3a6cccb75f85ffbae9e5e0cd5fe414
HoseungJang <hsjang.dev@gmail.com>
2020-10-11 15:47:40 +0900

    add test data

---
+++ src/data/projects.ts
@@ -0,0 +1,16 @@
+import { Project } from "../interfaces/Project";
+
+export const projects: Project[] = [
+  {
+    img: "",
+    name: "Reffering",
+    duration: "2020.08 ~ 2020.10",
+    description: "나만의 북마크 웹 서비스",
+    references: [
+      {
+        name: "프로젝트 일지",
+        link: "https://velog.io/@alvin/series/Reffering",
+      },
+    ],
+  },
+];
---
+++ src/data/skills.ts
@@ -0,0 +1,8 @@
+import { Skill } from "../interfaces/Skill";
+
+export const skills: Skill[] = [
+  {
+    img: "",
+    name: "React",
+  },
+];
