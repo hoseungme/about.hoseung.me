722034ba2a6e727f705ca49c69c588ebd8c4b4f6
HoseungJang <hsjang.dev@gmail.com>
2020-10-11 15:25:55 +0900

    add deployment config for github pages

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,7 @@ import { BrowserRouter } from "react-router-dom";
 import { Routes } from "./routes";
 
 ReactDOM.render(
-  <BrowserRouter>
+  <BrowserRouter basename="/resume">
     <Routes />
   </BrowserRouter>,
   document.getElementById("root")
