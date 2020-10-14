9d2206c3e95543e6db681fcaf0042fd516b8a0e1
HoseungJang <hsjang.dev@gmail.com>
2020-10-14 23:19:53 +0900

    add project card list container

--- src/components/Project/CardList.tsx
+++ src/components/Project/CardList.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import styled from "styled-components";
 
 import { ProjectCard } from "./Card";
 
@@ -8,10 +9,21 @@ export const ProjectCardList: React.FC<{ projects: Project[] }> = ({
   projects,
 }) => {
   return (
-    <>
+    <Container>
       {projects.map((p) => (
         <ProjectCard {...p} />
       ))}
-    </>
+    </Container>
   );
 };
+
+const Container = styled.div`
+  width: 100%;
+
+  display: flex;
+  flex-flow: row wrap;
+
+  > * {
+    width: 25%;
+  }
+`;
