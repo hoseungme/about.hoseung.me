21279f38a02350e108c4c30b53c786adb964bfb3
HoseungJang <hsjang.dev@gmail.com>
2020-10-15 00:41:45 +0900

    add card container

--- src/components/Project/Card.tsx
+++ src/components/Project/Card.tsx
@@ -1,6 +1,9 @@
 import React from "react";
+import styled from "styled-components";
 
 import { Project } from "../../interfaces/Project";
+
+import { Color } from "../../constants/Color";
 
 export const ProjectCard: React.FC<Project> = ({
   img,
@@ -9,12 +12,17 @@ export const ProjectCard: React.FC<Project> = ({
   description,
   references,
 }) => {
-  return (
-    <div>
-      <div>{img}</div>
-      <div>{name}</div>
-      <div>{duration}</div>
-      <div>{description}</div>
-    </div>
-  );
+  return <Container></Container>;
 };
+
+const Container = styled.div`
+  width: 100%;
+
+  display: flex;
+
+  overflow: hidden;
+
+  border-radius: 6px;
+
+  box-shadow: 0 0 15px ${Color.LightGrey};
+`;
