5dc797c8f9f174a55d06140955bf03565e859c02
HoseungJang <hsjang.dev@gmail.com>
2020-10-14 23:31:13 +0900

    add global styles

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
+import { createGlobalStyle } from "styled-components";
 
 import { Routes } from "./routes";
 
+const GlobalStyles = createGlobalStyle`
+  html, body {
+    width: 100%;
+    height: 100%;
+
+    margin: 0;
+    padding: 0;
+  }
+`;
+
 ReactDOM.render(
-  <BrowserRouter basename="/resume">
-    <Routes />
-  </BrowserRouter>,
+  <>
+    <GlobalStyles />
+    <BrowserRouter basename="/resume">
+      <Routes />
+    </BrowserRouter>
+  </>,
   document.getElementById("root")
 );
