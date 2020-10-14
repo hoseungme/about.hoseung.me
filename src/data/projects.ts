4bc69b27525c50c5375e3a790b140ae13d1d9b09
HoseungJang <hsjang.dev@gmail.com>
2020-10-14 23:15:56 +0900

    add more test data

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -2,7 +2,60 @@ import { Project } from "../interfaces/Project";
 
 export const projects: Project[] = [
   {
-    img: "",
+    img:
+      "https://media.vlpt.us/images/alvin/post/f46191fb-ce70-4482-93a8-817d17f5f119/Reffering.png",
+    name: "Reffering",
+    duration: "2020.08 ~ 2020.10",
+    description: "나만의 북마크 웹 서비스",
+    references: [
+      {
+        name: "프로젝트 일지",
+        link: "https://velog.io/@alvin/series/Reffering",
+      },
+    ],
+  },
+  {
+    img:
+      "https://media.vlpt.us/images/alvin/post/f46191fb-ce70-4482-93a8-817d17f5f119/Reffering.png",
+    name: "Reffering",
+    duration: "2020.08 ~ 2020.10",
+    description: "나만의 북마크 웹 서비스",
+    references: [
+      {
+        name: "프로젝트 일지",
+        link: "https://velog.io/@alvin/series/Reffering",
+      },
+    ],
+  },
+  {
+    img:
+      "https://media.vlpt.us/images/alvin/post/f46191fb-ce70-4482-93a8-817d17f5f119/Reffering.png",
+    name: "Reffering",
+    duration: "2020.08 ~ 2020.10",
+    description: "나만의 북마크 웹 서비스",
+    references: [
+      {
+        name: "프로젝트 일지",
+        link: "https://velog.io/@alvin/series/Reffering",
+      },
+    ],
+  },
+  {
+    img:
+      "https://media.vlpt.us/images/alvin/post/f46191fb-ce70-4482-93a8-817d17f5f119/Reffering.png",
+    name: "Reffering",
+    duration: "2020.08 ~ 2020.10",
+    description: "나만의 북마크 웹 서비스",
+    references: [
+      {
+        name: "프로젝트 일지",
+        link: "https://velog.io/@alvin/series/Reffering",
+      },
+    ],
+  },
+  {
+    img:
+      "https://media.vlpt.us/images/alvin/post/f46191fb-ce70-4482-93a8-817d17f5f119/Reffering.png",
     name: "Reffering",
     duration: "2020.08 ~ 2020.10",
     description: "나만의 북마크 웹 서비스",
