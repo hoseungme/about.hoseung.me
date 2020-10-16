fbc8a2b907da24f0a8eec81ec50de9661ef43ada
HoseungJang <hsjang.dev@gmail.com>
2020-10-16 22:39:19 +0900

    fix data

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -3,70 +3,126 @@ import { Project } from "../interfaces/Project";
 export const projects: Project[] = [
   {
     img:
-      "https://media.vlpt.us/images/alvin/post/f46191fb-ce70-4482-93a8-817d17f5f119/Reffering.png",
-    name: "Reffering",
+      "https://media.vlpt.us/images/alvin/post/657edced-a696-4f56-a2f9-eff2d5d929b1/Referring.png",
+    name: "오늘은 뭘 참조해볼까? 프로젝트 Referring",
     duration: "2020.08 ~ 2020.10",
     description: "나만의 북마크 웹 서비스",
     references: [
       {
         name: "프로젝트 일지",
-        link: "https://velog.io/@alvin/series/Reffering",
+        link: "https://velog.io/@alvin/series/Referring",
       },
     ],
     tags: ["Front End"],
   },
   {
     img:
-      "https://media.vlpt.us/images/alvin/post/f46191fb-ce70-4482-93a8-817d17f5f119/Reffering.png",
-    name: "Reffering",
+      "https://media.vlpt.us/images/alvin/post/657edced-a696-4f56-a2f9-eff2d5d929b1/Referring.png",
+    name: "오늘은 뭘 참조해볼까? 프로젝트 Referring",
     duration: "2020.08 ~ 2020.10",
     description: "나만의 북마크 웹 서비스",
     references: [
       {
         name: "프로젝트 일지",
-        link: "https://velog.io/@alvin/series/Reffering",
+        link: "https://velog.io/@alvin/series/Referring",
       },
     ],
     tags: ["Front End"],
   },
   {
     img:
-      "https://media.vlpt.us/images/alvin/post/f46191fb-ce70-4482-93a8-817d17f5f119/Reffering.png",
-    name: "Reffering",
+      "https://media.vlpt.us/images/alvin/post/657edced-a696-4f56-a2f9-eff2d5d929b1/Referring.png",
+    name: "오늘은 뭘 참조해볼까? 프로젝트 Referring",
     duration: "2020.08 ~ 2020.10",
     description: "나만의 북마크 웹 서비스",
     references: [
       {
         name: "프로젝트 일지",
-        link: "https://velog.io/@alvin/series/Reffering",
+        link: "https://velog.io/@alvin/series/Referring",
       },
     ],
     tags: ["Front End"],
   },
   {
     img:
-      "https://media.vlpt.us/images/alvin/post/f46191fb-ce70-4482-93a8-817d17f5f119/Reffering.png",
-    name: "Reffering",
+      "https://media.vlpt.us/images/alvin/post/657edced-a696-4f56-a2f9-eff2d5d929b1/Referring.png",
+    name: "오늘은 뭘 참조해볼까? 프로젝트 Referring",
     duration: "2020.08 ~ 2020.10",
     description: "나만의 북마크 웹 서비스",
     references: [
       {
         name: "프로젝트 일지",
-        link: "https://velog.io/@alvin/series/Reffering",
+        link: "https://velog.io/@alvin/series/Referring",
       },
     ],
     tags: ["Front End"],
   },
   {
     img:
-      "https://media.vlpt.us/images/alvin/post/f46191fb-ce70-4482-93a8-817d17f5f119/Reffering.png",
-    name: "Reffering",
+      "https://media.vlpt.us/images/alvin/post/657edced-a696-4f56-a2f9-eff2d5d929b1/Referring.png",
+    name: "오늘은 뭘 참조해볼까? 프로젝트 Referring",
     duration: "2020.08 ~ 2020.10",
     description: "나만의 북마크 웹 서비스",
     references: [
       {
         name: "프로젝트 일지",
-        link: "https://velog.io/@alvin/series/Reffering",
+        link: "https://velog.io/@alvin/series/Referring",
+      },
+    ],
+    tags: ["Front End"],
+  },
+  {
+    img:
+      "https://media.vlpt.us/images/alvin/post/657edced-a696-4f56-a2f9-eff2d5d929b1/Referring.png",
+    name: "오늘은 뭘 참조해볼까? 프로젝트 Referring",
+    duration: "2020.08 ~ 2020.10",
+    description: "나만의 북마크 웹 서비스",
+    references: [
+      {
+        name: "프로젝트 일지",
+        link: "https://velog.io/@alvin/series/Referring",
+      },
+    ],
+    tags: ["Front End"],
+  },
+  {
+    img:
+      "https://media.vlpt.us/images/alvin/post/657edced-a696-4f56-a2f9-eff2d5d929b1/Referring.png",
+    name: "오늘은 뭘 참조해볼까? 프로젝트 Referring",
+    duration: "2020.08 ~ 2020.10",
+    description: "나만의 북마크 웹 서비스",
+    references: [
+      {
+        name: "프로젝트 일지",
+        link: "https://velog.io/@alvin/series/Referring",
+      },
+    ],
+    tags: ["Front End"],
+  },
+  {
+    img:
+      "https://media.vlpt.us/images/alvin/post/657edced-a696-4f56-a2f9-eff2d5d929b1/Referring.png",
+    name: "오늘은 뭘 참조해볼까? 프로젝트 Referring",
+    duration: "2020.08 ~ 2020.10",
+    description: "나만의 북마크 웹 서비스",
+    references: [
+      {
+        name: "프로젝트 일지",
+        link: "https://velog.io/@alvin/series/Referring",
+      },
+    ],
+    tags: ["Front End"],
+  },
+  {
+    img:
+      "https://media.vlpt.us/images/alvin/post/657edced-a696-4f56-a2f9-eff2d5d929b1/Referring.png",
+    name: "오늘은 뭘 참조해볼까? 프로젝트 Referring",
+    duration: "2020.08 ~ 2020.10",
+    description: "나만의 북마크 웹 서비스",
+    references: [
+      {
+        name: "프로젝트 일지",
+        link: "https://velog.io/@alvin/series/Referring",
       },
     ],
     tags: ["Front End"],
