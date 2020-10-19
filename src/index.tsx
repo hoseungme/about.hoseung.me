3915fc50ba388431fb2cf7b70c5bed3618a2603e
HoseungJang <hsjang.dev@gmail.com>
2020-10-20 00:50:43 +0900

    add web font

--- src/index.tsx
+++ src/index.tsx
@@ -13,7 +13,8 @@ const GlobalStyles = createGlobalStyle`
     margin: 0;
     padding: 0;
 
-    font-family: sans-serif;
+    font-family: "Noto Sans KR", sans-serif;
+    font-weight: 100;
   }
 `;
 
