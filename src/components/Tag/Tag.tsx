08c35b46d4cd05cc76f0152efc7de4869ffb7fda
HoseungJang <hsjang.dev@gmail.com>
2020-10-19 23:23:14 +0900

    fix linter warning

--- src/components/Tag/Tag.tsx
+++ src/components/Tag/Tag.tsx
@@ -1,8 +1,6 @@
 import React from "react";
 import styled from "styled-components";
 
-import { Project } from "../../interfaces/Project";
-
 import { Color } from "../../constants/Color";
 
 export const Tag: React.FC<{ tag: string }> = ({ tag }) => {
