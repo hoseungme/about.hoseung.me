fe0751197507c04f14785f853d4c00e2cd387c34
HoseungJang <hsjang.dev@gmail.com>
2020-10-21 00:59:11 +0900

    main wip

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,37 +1,18 @@
 import React from "react";
-import { Header } from "../../components/Header/Header";
 
+import { Header } from "../../components/Header/Header";
 import { Profile } from "../../components/Profile/Profile";
 // import { ProjectCardList } from "../../components/Project/CardList";
 
+import { accounts } from "../../data/accounts";
+import { profile } from "../../data/profile";
 // import { projects } from "../../data/projects";
-
-import profile from "../../images/profile.png";
 
 export const Main: React.FC = () => {
   return (
     <>
-      <Header />
-      <Profile
-        profileImage={profile}
-        motto="기록은 나를 빛낸다"
-        comment="서브 프로젝트를 사랑하는 웹 개발자 입니다."
-      />
-      <Profile
-        profileImage={profile}
-        motto="기록은 나를 빛낸다"
-        comment="서브 프로젝트를 사랑하는 웹 개발자 입니다."
-      />
-      <Profile
-        profileImage={profile}
-        motto="기록은 나를 빛낸다"
-        comment="서브 프로젝트를 사랑하는 웹 개발자 입니다."
-      />
-      <Profile
-        profileImage={profile}
-        motto="기록은 나를 빛낸다"
-        comment="서브 프로젝트를 사랑하는 웹 개발자 입니다."
-      />
+      <Header accounts={accounts} />
+      <Profile {...profile} />
     </>
   );
 
