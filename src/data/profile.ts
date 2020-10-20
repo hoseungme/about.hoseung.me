8e72ad1183bb1bb3fbaa1c499e806194b57987e1
HoseungJang <hsjang.dev@gmail.com>
2020-10-21 00:58:41 +0900

    add profile data

---
+++ src/data/profile.ts
@@ -0,0 +1,9 @@
+import { Profile } from "../interfaces/Profile";
+
+import profileImage from "../images/profile.png";
+
+export const profile: Profile = {
+  profileImage,
+  motto: "기록은 나를 빛낸다.",
+  comment: "서브 프로젝트를 사랑하는 웹 개발자 입니다.",
+};
---
+++ src/interfaces/Profile.ts
@@ -0,0 +1,5 @@
+export interface Profile {
+  profileImage: string;
+  motto: string;
+  comment: string;
+}
