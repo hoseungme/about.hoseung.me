1e2bb3971596f16bd357bd2d9f3487f2f52e1ce5
HoseungJang <hsjang.dev@gmail.com>
2020-10-28 00:30:59 +0900

    fix profile data

--- src/data/profile.ts
+++ src/data/profile.ts
@@ -4,6 +4,7 @@ import profileImage from "../images/profile/profile.png";
 
 export const profile: IProfile = {
   profileImage,
-  motto: "기록은 나를 빛낸다.",
-  comment: "안녕하세요. 서브 프로젝트를 사랑하는 웹 개발자 장호승 입니다.",
+  motto: "기록은 나를 빛낸다.😎",
+  comment:
+    "안녕하세요. 꾸준함을 좋아하는 개발자 장호승 입니다. 모든 것은 새로움의 연속이라고 생각하고, 변화에 빠르게 적응하는 개발자가 되기 위해 노력하고 있습니다.",
 };
