5763e582d2131a47be380e17025782e9ab2e6b99
HoseungJang <hsjang.dev@gmail.com>
2020-11-04 21:18:15 +0900

    adjust y position of shadow

--- src/components/Profile/index.tsx
+++ src/components/Profile/index.tsx
@@ -96,7 +96,7 @@ const Container = styled.section`
 
         border-radius: 50%;
 
-        box-shadow: 0 -5px 20px ${Color.LightGrey};
+        box-shadow: 0 0 20px ${Color.LightGrey};
       }
     }
   }
