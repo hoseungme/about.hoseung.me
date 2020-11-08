3dba583fdd9fc289ec23a4e18308866fe1977e7f
HoseungJang <hsjang.dev@gmail.com>
2020-11-08 19:12:52 +0900

    add useValueByMedia hook

---
+++ src/hooks/useValueByMedia.ts
@@ -0,0 +1,19 @@
+import { useMemo } from "react";
+import { useMediaLayout } from "use-media";
+
+import { Device } from "../constants/Device";
+
+type Values<T> = {
+  desktop: T;
+  tablet: T;
+  mobile: T;
+};
+
+export const useValueByMedia = <T>(values: Values<T>) => {
+  const isDesktop = useMediaLayout({ minWidth: Device.Tablet + 1 });
+  const isTablet = useMediaLayout({ minWidth: Device.Mobile + 1 });
+
+  const currentMedia = isDesktop ? "desktop" : isTablet ? "tablet" : "mobile";
+
+  return values[currentMedia];
+};
