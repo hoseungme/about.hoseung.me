326846f9c061a4e9f3654661f3e9f9eebe3def18
HoseungJang <hsjang.dev@gmail.com>
2020-11-10 21:24:25 +0900

    add deep dark white

--- src/constants/Color.ts
+++ src/constants/Color.ts
@@ -1,9 +1,10 @@
 export const Color = {
   LightGrey: "#BDBDBD",
   FontBlack: "#585858",
-  White: "#ffffff",
+  White: "#FFFFFF",
   Black: "#000000",
-  Mint: "#00d1b2",
+  Mint: "#00D1B2",
   DarkMint: "#00B99D",
   DarkWhite: "#FAFAFA",
+  DeepDarkWhite: "#F6F6F6",
 };
