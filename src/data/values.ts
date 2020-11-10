1d9220bdf9020edb4f8bb03ae2c8cbf59b53604a
HoseungJang <hsjang.dev@gmail.com>
2020-11-10 21:25:09 +0900

    add values data

---
+++ src/data/values.ts
@@ -0,0 +1,19 @@
+import { IValues } from "../interfaces/Values";
+
+export const values: IValues[] = [
+  {
+    title: "기록을 좋아합니다.",
+    description:
+      "기록은 스스로를 기억하고, 돌아보고, 증명할 수 있는 좋은 방법 중 하나라고 생각하고, 소통에도 큰 도움을 줄 수 있다고 생각합니다.",
+  },
+  {
+    title: "배움에 개방적입니다.",
+    description:
+      "무언가를 배우고 싶어지거나 혹은 배워야 한다면, 시간을 지체하지 않습니다.",
+  },
+  {
+    title: "꾸준함을 좋아합니다.",
+    description:
+      "한 번에 많은 노력을 들이는 것 보단 조금씩이라도 꾸준히 계속해오는 것을 좋아합니다.",
+  },
+];
