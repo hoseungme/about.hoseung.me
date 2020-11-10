6a63ed8e1e842da81dd2869af11b0ea85112a5a4
HoseungJang <hsjang.dev@gmail.com>
2020-11-10 19:40:57 +0900

    add profile link button

---
+++ src/components/Profile/ProfileLink.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+import styled from "styled-components";
+import { Color } from "../../constants/Color";
+
+import { IProfile } from "../../interfaces/Profile";
+
+export const ProfileLink: React.FC<IProfile["links"][0]> = ({ text, link }) => {
+  return (
+    <Container
+      onClick={() => {
+        window.location.href = link;
+      }}
+    >
+      {text}
+    </Container>
+  );
+};
+
+const Container = styled.button`
+  padding: 4px 8px;
+
+  border: 1px solid ${Color.DarkMint};
+  border-radius: 5px;
+
+  background-color: ${Color.DarkMint};
+
+  font-size: 1.1rem;
+  color: ${Color.White};
+
+  cursor: pointer;
+
+  outline: none;
+
+  transition: color 0.5s, background-color 0.5s;
+
+  &:not(:last-child) {
+    margin-right: 10px;
+  }
+
+  &:hover {
+    background-color: ${Color.DarkWhite};
+
+    color: ${Color.DarkMint};
+  }
+`;
