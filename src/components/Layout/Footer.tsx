96c470f7ccad84bdb32c83182617ddb4bb5a2386
HoseungJang <hsjang.dev@gmail.com>
2020-11-12 22:44:26 +0900

    add span wrap

--- src/components/Layout/Footer.tsx
+++ src/components/Layout/Footer.tsx
@@ -2,13 +2,16 @@ import React from "react";
 import styled from "styled-components";
 
 import { Color } from "../../constants/Color";
+import { Device } from "../../constants/Device";
 
 export const Footer: React.FC = () => {
   return (
     <Container>
-      <span>ⓒ 2020</span>
-      <a href="https://github.com/HoseungJang/resume">HoseungJang</a>
-      <span>All RIGHTS RESERVED.</span>
+      <span>
+        ⓒ 2020
+        <a href="https://github.com/HoseungJang/resume">HoseungJang</a>
+        All RIGHTS RESERVED.
+      </span>
     </Container>
   );
 };
@@ -17,32 +20,34 @@ const Container = styled.footer`
   width: 100%;
   height: 150px;
 
-  display: flex;
+  display: inline-flex;
   flex-flow: row wrap;
   justify-content: center;
   align-items: center;
 
-  font-size: 1.1rem;
-  font-weight: 500;
-
-  color: ${Color.DarkMint};
-
   background-color: ${Color.LightMint};
 
-  > a {
-    margin: 0 8px;
-
-    font-size: 1.2rem;
-    font-weight: 700;
+  > span {
+    font-size: 1.1rem;
+    font-weight: 500;
 
     color: ${Color.DarkMint};
 
-    text-decoration: none;
+    > a {
+      margin: 0 8px;
 
-    cursor: pointer;
-  }
+      font-size: 1.2rem;
+      font-weight: 700;
 
-  > * {
-    flex-shrink: 0;
+      color: ${Color.DarkMint};
+
+      text-decoration: none;
+
+      cursor: pointer;
+    }
+
+    > * {
+      flex-shrink: 0;
+    }
   }
 `;
