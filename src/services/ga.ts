e00ba4e096d6d41f144eb31a69a15d4ea11e75b9
HoseungJang <hsjang.dev@gmail.com>
2020-11-14 01:03:52 +0900

    add google analytics service

---
+++ src/services/ga.ts
@@ -0,0 +1,31 @@
+import ReactGA from "react-ga";
+
+export class GAService {
+  private env: "development" | "production" | "test";
+
+  constructor() {
+    this.env = process.env.NODE_ENV;
+
+    if (!process.env.REACT_APP_GA_TRACKING_ID) {
+      throw new Error("GA_TRACKING_ID must be provided.");
+    }
+
+    ReactGA.initialize(process.env.REACT_APP_GA_TRACKING_ID);
+  }
+
+  private get isProduction() {
+    return this.env === "production";
+  }
+
+  public trackPageView(path: string) {
+    if (!this.isProduction) {
+      console.log("page viewed");
+
+      return;
+    }
+
+    ReactGA.pageview(path);
+  }
+}
+
+export const GA = new GAService();
