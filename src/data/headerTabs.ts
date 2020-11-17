5455852d9ca5511f0bf71064950b51ba22a41761
HoseungJang <hsjang.dev@gmail.com>
2020-11-17 21:55:28 +0900

    add header tabs data

--- src/data/headerTabs.ts
+++ src/data/headerTabs.ts
@@ -1,9 +1,23 @@
+import { IoLogoGithub } from "react-icons/io";
+import { AiFillMediumSquare } from "react-icons/ai";
+import { FaBook } from "react-icons/fa";
+
 import { IHeaderTab } from "../interfaces/HeaderTab";
 
 export const headerTabs: IHeaderTab[] = [
   {
-    icon: "IoLogoGithub",
-    text: "Github",
+    icon: IoLogoGithub,
+    text: "github",
     link: "https://github.com/HoseungJang",
+  },
+  {
+    icon: AiFillMediumSquare,
+    text: "medium",
+    link: "https://medium.com/@HoseungJang",
+  },
+  {
+    icon: FaBook,
+    text: "velog",
+    link: "https://velog.io/@alvin/series",
   },
 ];
