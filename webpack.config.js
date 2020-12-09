f0cb46d0e7bbd782c8d927c56526f21706bf6f84
HoseungJang <hsjang.dev@gmail.com>
2020-12-09 20:16:56 +0900

    add webpack config

---
+++ webpack.config.js
@@ -0,0 +1,68 @@
+const path = require("path");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const webpack = require("webpack");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+module.exports = (env) => {
+  const { mode } = env;
+  const isProduction = mode === "production";
+
+  const devServer = !isProduction ? {
+    contentBase: path.resolve("./build"),
+    index: "index.html",
+    port: 3000,
+    open: true,
+  } : undefined;
+
+  return {
+    entry: "./src/index.tsx",
+    output: {
+      filename: "bundle.js",
+      path: path.resolve("./build"),
+    },
+    resolve: {
+      fallback: {
+        fs: false,
+        path: false,
+      },
+      modules: ["node_modules"],
+      extensions: [".ts", ".tsx", ".js"],
+    },
+    devServer,
+    mode,
+    module: {
+      rules: [
+        {
+          test: /\.png$/,
+          use: [
+            {
+              loader: "url-loader",
+              options: {
+                limit: 10000,
+                name: 'static/[name].[hash:8].[ext]',
+              },
+            },
+          ],
+        },
+        {
+          test: /\.(ts|tsx)$/,
+          use: ["babel-loader", "ts-loader"],
+        },
+      ],
+    },
+    plugins: [
+      new HtmlWebPackPlugin({
+        template: "./public/index.html",
+        filename: "index.html",
+      }),
+      new webpack.EnvironmentPlugin({
+        NODE_ENV: mode,
+        ...process.env,
+      }),
+      new CleanWebpackPlugin(),
+    ],
+  };
+};
