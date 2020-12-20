c57c0e214dfbeda688e20281d85b7a58e9b138c8
HoseungJang <hsjang.dev@gmail.com>
2020-12-20 19:58:18 +0900

    add details of project LPL

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,11 +1,40 @@
 import { IProject } from "../interfaces/Project";
 
+import lplImage from "../images/project/lpl.png";
 import resumeImage from "../images/project/resume.png";
 import referringImage from "../images/project/referring.png";
 import coronaSolutionImage from "../images/project/corona-solution.png";
 import dsmMarketImage from "../images/project/dsm-market.png";
 
 export const projects: IProject[] = [
+  {
+    img: lplImage,
+    title: "듣고싶은 음악을 한눈에. LPL",
+    duration: "2020.12 ~ 2020.12",
+    description:
+      "LPL은 Listing, Playing, Listening에서 유래한 이름으로, 간단한 플레이리스트 웹 사이트입니다. 회사에서 음악을 들으려고 유튜브를 켜는게 눈치가 보여서, 직접 만든 플레이리스트를 사용해보자는 생각으로 시작하게 되었습니다.",
+    activities: [
+      "Youtube data API, Local Storage를 활용한 동영상 정보 저장",
+      "react-player를 사용한 유튜브 동영상 컨트롤",
+      "CRA에서 벗어나 바벨, 웹팩, prettier, eslint 등 전체적인 프론트엔드 개발환경을 직접 커스터마이징",
+    ],
+    references: [
+      {
+        text: "프로젝트 일지",
+        link:
+          "https://velog.io/@alvin/series/%ED%94%84%EB%A1%9C%EC%A0%9D%ED%8A%B8-LPL",
+      },
+      {
+        text: "프론트엔드 코드",
+        link: "https://github.com/HoseungJang/LPL",
+      },
+      {
+        text: "서비스 이용해보기",
+        link: "https://hoseungjang.github.io/LPL/",
+      },
+    ],
+    tags: ["Front-end"],
+  },
   {
     img: resumeImage,
     title: "나를 소개합니다. Résumé V1",
@@ -15,7 +44,7 @@ export const projects: IProject[] = [
     activities: [
       "다른 레주메 사이트, 회사 소개 페이지, 개인 블로그 등을 참고하여 깔끔한 UI와 부드러운 인터랙션에 대해 학습",
       "Intersection Observer를 적극 활용한 UI 구현",
-      "전역 컨텍스트를 사용한 모달 인터페이스 구현",
+      "React Context를 활용한 전역 모달 인터페이스 구현",
       "AWS S3, CloudFront를 사용해서 배포",
       "Google Analytics를 활용한 유저 행동 트래킹",
     ],
