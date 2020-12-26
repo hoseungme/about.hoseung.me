c3493416f35e886d7277a48c2c0a490a294ca6fb
HoseungJang <hsjang.dev@gmail.com>
2020-12-26 16:22:08 +0900

    apply padding

--- src/components/Experience/ExperienceSection.tsx
+++ src/components/Experience/ExperienceSection.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styled from "styled-components";
+import { Color } from "../../constants/Color";
 import { Device } from "../../constants/Device";
 import { experiences } from "../../data/section/experiences";
 
@@ -19,18 +20,22 @@ export const ExperienceSection: React.FC<{
 const Container = styled.section`
   width: 100%;
 
+  padding: 200px 0;
+
   display: flex;
   justify-content: center;
 
   @media (max-width: ${Device.Tablet}px) {
     justify-content: flex-start;
 
+    padding: 150px 0;
     margin-left: 30%;
   }
 
   @media (max-width: ${Device.Mobile}px) {
     justify-content: flex-start;
 
+    padding: 100px 0;
     margin-left: 8%;
   }
 `;
