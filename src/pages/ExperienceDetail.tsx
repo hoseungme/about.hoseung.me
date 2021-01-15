52be0eba914390f19b6c3df149af67d1c9bf82a8
HoseungJang <hsjang.dev@gmail.com>
2021-01-16 00:25:58 +0900

    apply markdown parser

--- src/pages/ExperienceDetail.tsx
+++ src/pages/ExperienceDetail.tsx
@@ -1,8 +1,21 @@
 import React from "react";
 import styled from "styled-components";
+import ReactMarkdown from "react-markdown";
+import { RouteComponentProps } from "react-router-dom";
 
-export const ExperienceDetail: React.FC = () => {
-  return <Container>hello</Container>;
+type LocationState = {
+  content: string;
+};
+
+export const ExperienceDetail: React.FC<RouteComponentProps> = ({
+  location,
+}) => {
+  const { content } = location.state as LocationState;
+  return (
+    <Container>
+      <ReactMarkdown>{content}</ReactMarkdown>
+    </Container>
+  );
 };
 
 const Container = styled.main``;
