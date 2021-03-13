41f42b424b0c740acf7b6b4ace2b5ed49d459415
HoseungJang <hsjang.dev@gmail.com>
2021-03-14 00:10:54 +0900

    update github link

--- src/data/section/projects.ts
+++ src/data/section/projects.ts
@@ -63,7 +63,7 @@ export const projects: IProject[] = [
       },
       {
         text: "프론트엔드 코드",
-        link: "https://github.com/hoseung-only/about.janghoseung.com",
+        link: "https://github.com/hoseung-only/about.hoseung.me",
       },
     ],
     tags: ["Front-end"],
