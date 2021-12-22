97c994042539e2445d4ccd355bd8167a508b984e
HoseungJang <hsjang.dev@gmail.com>
2021-12-23 04:28:36 +0900

    add Font

---
+++ src/components/common/Font/index.tsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const Font = {
+  R: styled.div`
+    font-weight: 300;
+  `,
+  M: styled.div`
+    font-weight: 500;
+  `,
+  B: styled.div`
+    font-weight: 700;
+  `,
+};
