db22da6748d96941fb53eaf93177677213dda387
HoseungJang <hsjang.dev@gmail.com>
2021-12-23 13:27:42 +0900

    add toLowerCase

--- src/helpers/common/renderHash.ts
+++ src/helpers/common/renderHash.ts
@@ -2,5 +2,8 @@
  * input 문자열을 hash 라우팅을 위한 HTML id attribute 값으로 변환
  */
 export function renderHash(input: string) {
-  return input.replace(/\s/g, "-").replace(/[^가-힣A-Za-z0-9-]/g, "");
+  return input
+    .toLowerCase()
+    .replace(/\s/g, "-")
+    .replace(/[^가-힣a-z0-9-]/g, "");
 }
