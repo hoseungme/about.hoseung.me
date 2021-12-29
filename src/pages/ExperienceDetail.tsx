452af7a4b0378b1d82d112f248ba151ec08c4ebd
HoseungJang <hsjang.dev@gmail.com>
2021-12-30 01:47:39 +0900

    remove back button

--- src/pages/ExperienceDetail.tsx
+++ src/pages/ExperienceDetail.tsx
@@ -1,6 +1,5 @@
 import { useEffect } from "react";
-import { BiArrowBack } from "react-icons/bi";
-import { useParams, Link } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import { useInView } from "react-intersection-observer";
 import styled from "styled-components";
 
@@ -8,7 +7,6 @@ import { GA } from "../services/ga";
 
 import { experienceDetailMap } from "../data/experience";
 
-import { Color } from "../constants/Color";
 import { Media } from "../constants/Media";
 
 import { Markdown } from "../components/experience/Markdown";
@@ -26,13 +24,7 @@ export function ExperienceDetail() {
 
   return (
     <Container>
-      <Link className="back-button" to="/">
-        <BiArrowBack />
-        메인으로 돌아가기
-      </Link>
-      <div className="content">
-        <Markdown content={experienceDetailMap.get(id)!} />
-      </div>
+      <Markdown content={experienceDetailMap.get(id)!} />
       <div ref={bottomRef} />
     </Container>
   );
@@ -43,28 +35,6 @@ const Container = styled.main`
   flex-direction: column;
 
   padding: 0 25% 20px;
-
-  > .back-button {
-    display: flex;
-    align-items: center;
-
-    margin-top: 20px;
-
-    font-size: 1.3rem;
-    font-weight: 500;
-    color: ${Color.Blue};
-    text-decoration: none;
-
-    transition: color 0.1s;
-
-    &:hover {
-      color: ${Color.BlueHover};
-    }
-
-    > :first-child {
-      margin-right: 4px;
-    }
-  }
 
   ${Media.Tablet} {
     padding: 0 20% 20px;
