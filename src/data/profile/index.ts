48da2841a929baf9d3efbc459377b3e6816e3b86
HoseungJang <hsjang.dev@gmail.com>
2021-12-29 22:38:19 +0900

    fix profile data

--- src/data/profile/index.ts
+++ src/data/profile/index.ts
@@ -7,6 +7,7 @@ export interface ProfileData {
 export const profileData: ProfileData = {
   title: "안녕하세요. 좋은 경험을 추구하는 개발자 장호승입니다.",
   descriptions: [
+    "주어진 작업의 이유를 중요하게 생각합니다.",
     "기록하고 돌아보고 개선하는 과정을 좋아합니다.",
     "자동화로 생산성을 높히는 것에 관심이 많습니다.",
     "하고싶은 일에 도전해볼 수 있는 조직을 좋아합니다.",
