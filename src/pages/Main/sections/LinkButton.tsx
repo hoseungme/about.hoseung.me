f654f170ee3b4878c751dae17fe667e30a701039
HoseungJang <hsjang.dev@gmail.com>
2022-03-06 07:43:49 +0900

    add LinkButton

---
+++ src/pages/Main/sections/LinkButton.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+interface LinkButtonProps {
+  className?: string;
+  to: string;
+}
+
+export function LinkButton({
+  className,
+  to,
+  children,
+}: React.PropsWithChildren<LinkButtonProps>) {
+  if (/^(http|https)/.test(to)) {
+    return (
+      <Container as="a" className={className} href={to}>
+        {children}
+      </Container>
+    );
+  }
+
+  return (
+    <Container className={className} to={to}>
+      {children}
+    </Container>
+  );
+}
+
+const Container = styled(Link)`
+  display: flex;
+  align-items: center;
+  padding: 0.5em 1em;
+  border-radius: 12px;
+  background-color: var(--grey0);
+  transition: background-color 0.2s;
+
+  &:hover {
+    background-color: var(--grey1);
+  }
+`;
