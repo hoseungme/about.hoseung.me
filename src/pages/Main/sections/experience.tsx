6e51d2dcd1815e67d890b5519fc8f8773c90a1b5
HoseungJang <hsjang.dev@gmail.com>
2022-03-06 08:11:22 +0900

    fix import grouping

--- src/pages/Main/sections/experience.tsx
+++ src/pages/Main/sections/experience.tsx
@@ -1,8 +1,8 @@
 import styled from "styled-components";
 
 import { FadeIn } from "../../../components/common/FadeIn";
-import { LinkButton } from "./LinkButton";
 
+import { LinkButton } from "./LinkButton";
 import { SectionTitle } from "./SectionTitle";
 
 export function ExperienceSection() {
