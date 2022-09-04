dae056a0f498d287d3d1500c355bd29602639b56
HoseungJang <hsjang.dev@gmail.com>
2022-09-04 19:29:19 +0900

    apply dark theme

--- src/index.tsx
+++ src/index.tsx
@@ -11,6 +11,7 @@ import { RouteSwitch } from "./pages";
 const GlobalStyle = createGlobalStyle`
   :root {
     --white0: rgba(255, 255, 255, 1);
+    --white0_t70: rgba(255, 255, 255, 0.7);
 
     --black0: rgba(85, 87, 112, 1);
     --black1: rgba(0, 0, 0, 1);
@@ -24,6 +25,24 @@ const GlobalStyle = createGlobalStyle`
     --green1: rgba(0, 204, 173, 1);
     --green2: rgba(0, 179, 152, 1);
     --green3: rgba(0, 153, 130, 1);
+
+    @media (prefers-color-scheme: dark) {
+      --white0: rgba(36, 36, 36, 1);
+      --white0_t70: rgba(36, 36, 36, 0.7);
+
+      --black0: rgba(216, 216, 216, 1);
+      --black1: rgba(189, 189, 189, 1);
+
+      --grey0: rgba(110, 110, 110, 1);
+      --grey1: rgba(100, 100, 100, 1);
+      --grey2: rgba(90, 90, 90, 1);
+      --grey3: rgba(80, 80, 80, 1);
+  
+      --green0: rgba(0, 230, 195, 1);
+      --green1: rgba(0, 204, 173, 1);
+      --green2: rgba(0, 179, 152, 1);
+      --green3: rgba(0, 153, 130, 1);
+    }
   }
 
   html, body {
