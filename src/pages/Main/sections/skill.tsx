7056ffef503e800579d970abe036278c0b992aa0
HoseungJang <hsjang.dev@gmail.com>
2023-02-15 05:38:11 +0900

    fix

--- src/pages/Main/sections/skill.tsx
+++ src/pages/Main/sections/skill.tsx
@@ -13,43 +13,40 @@ export function SkillSection() {
       <ul>
         <FadeIn as="li">
           <p className="font-regular">
-            Javascript ES6+와 <Highlight>Typescript</Highlight>에 익숙합니다.{" "}
-            <Highlight>타입 추론</Highlight>을 적극 활용하여{" "}
-            <Highlight>개발자 경험을 개선</Highlight>합니다.
+            <Highlight>Javascript ES6+</Highlight>와{" "}
+            <Highlight>Typescript</Highlight>에 익숙합니다.
           </p>
         </FadeIn>
         <FadeIn as="li">
           <p className="font-regular">
-            <Highlight>React</Highlight>를 사용한 CSR 기반 프론트엔드 개발에
-            익숙합니다. <Highlight>React의 렌더링 성능</Highlight>을 개선한
-            경험이 있습니다.
+            <Highlight>React</Highlight> 기반의{" "}
+            <Highlight>CSR/SSR 프론트엔드 개발</Highlight>에 익숙하고, 리렌더링
+            최적화, 번들 경량화 등 기본적인{" "}
+            <Highlight>웹 성능 최적화</Highlight> 경험이 있습니다.
           </p>
         </FadeIn>
         <FadeIn as="li">
           <p className="font-regular">
-            <Highlight>Node.js</Highlight>와{" "}
-            <Highlight>AWS의 서버리스 인프라</Highlight>를 활용한 백엔드 API
-            개발, 데이터 파이프라인 구축, SEO 처리 경험이 있습니다.{" "}
-            <Highlight>Github Actions</Highlight>로 CI/CD를 자동화하는 것에
+            <Highlight>Node.js의 Module System</Highlight>인 CJS/ESM을 이해하고,
+            두 Module System 모두에 호환 가능한 라이브러리를 운영 해본 경험이
+            있습니다.
+          </p>
+        </FadeIn>
+        <FadeIn as="li">
+          <p className="font-regular">
+            Node.js 기반의 <Highlight>다양한 백엔드 경험</Highlight>이 있습니다.
+            Lambda, DynamoDB 등 <Highlight>AWS의 서버리스 인프라</Highlight>에
+            익숙하고,{" "}
+            <Highlight>프론트엔드에 필요한 수준의 서버 또는 인프라</Highlight>를
+            구성하고 유지보수할 수 있습니다.
+          </p>
+        </FadeIn>
+        <FadeIn as="li">
+          <p className="font-regular">
+            <Highlight>웹뷰 환경</Highlight>의 모바일 웹 제품 개발 경험이
+            있습니다. User-Agent 파싱, 앱브릿지 통신, meta 태그 파싱 등{" "}
+            <Highlight>네이티브와 웹이 상호작용하는 패턴</Highlight>에 매우
             익숙합니다.
-          </p>
-        </FadeIn>
-        <FadeIn as="li">
-          <p className="font-regular">
-            <Highlight>Lighthouse CI</Highlight> 지표를 활용해 여러 방식으로
-            웹을 최적화해본 경험이 있습니다.
-          </p>
-        </FadeIn>
-        <FadeIn as="li">
-          <p className="font-regular">
-            <Highlight>웹뷰</Highlight> 환경을 위한 User Agent 처리, Applike UI
-            등 다양한 시도를 해본 경험이 있습니다.
-          </p>
-        </FadeIn>
-        <FadeIn as="li">
-          <p className="font-regular">
-            <Highlight>프론트엔드, 백엔드 전체</Highlight>에 걸친 설계, 개발,
-            유지보수를 해본 경험이 있습니다.
           </p>
         </FadeIn>
       </ul>
