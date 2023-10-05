7312ce7732ee23458c7dd1ba303a38fc41d60800
HoseungJang <hsjang.dev@gmail.com>
2023-10-06 05:23:44 +0900

    fix skill

--- src/pages/Main/sections/skill.tsx
+++ src/pages/Main/sections/skill.tsx
@@ -19,10 +19,16 @@ export function SkillSection() {
         </FadeIn>
         <FadeIn as="li">
           <p className="font-regular">
-            <Highlight>React</Highlight> 기반의{" "}
-            <Highlight>CSR/SSR 프론트엔드 개발</Highlight>에 익숙하고, 리렌더링
-            최적화, 번들 경량화 등 기본적인{" "}
+            <Highlight>React 기반의 CSR/SSR 프론트엔드 개발</Highlight>에
+            익숙하고, 리렌더링 최적화, 번들 경량화 등 기본적인{" "}
             <Highlight>웹 성능 최적화</Highlight> 경험이 있습니다.
+          </p>
+        </FadeIn>
+        <FadeIn as="li">
+          <p className="font-regular">
+            <Highlight>Node.js 기반의 백엔드 개발</Highlight> 경험이 있습니다.
+            Lambda, DynamoDB 등 <Highlight>AWS의 서버리스 인프라</Highlight>에
+            익숙합니다.
           </p>
         </FadeIn>
         <FadeIn as="li">
@@ -30,15 +36,6 @@ export function SkillSection() {
             <Highlight>Node.js의 Module System</Highlight>인 CJS/ESM을 이해하고,
             두 Module System 모두에 호환 가능한 라이브러리를 운영 해본 경험이
             있습니다.
-          </p>
-        </FadeIn>
-        <FadeIn as="li">
-          <p className="font-regular">
-            Node.js 기반의 <Highlight>다양한 백엔드 경험</Highlight>이 있습니다.
-            Lambda, DynamoDB 등 <Highlight>AWS의 서버리스 인프라</Highlight>에
-            익숙하고,{" "}
-            <Highlight>프론트엔드에 필요한 수준의 서버 또는 인프라</Highlight>를
-            구성하고 유지보수할 수 있습니다.
           </p>
         </FadeIn>
         <FadeIn as="li">
