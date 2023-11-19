cd386c0477e95ad4f7eb8b6b984c4dc28261b6ba
HoseungJang <hsjang.dev@gmail.com>
2023-11-20 03:52:04 +0900

    fix ga setting

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,28 +1,12 @@
 import React from "react";
-import { useHistory, Switch, Route } from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
 
 import { MainKo } from "./Main/Ko";
 import { MainEn } from "./Main/En";
 import { ExperienceKo } from "./Experience/Ko";
 import { ExperienceEn } from "./Experience/En";
 
-import { GA } from "services/ga";
-
 export function RouteSwitch() {
-  const history = useHistory();
-
-  React.useEffect(() => {
-    GA.trackPageView({ path: window.location.pathname });
-
-    const unlisten = history.listen((location, action) => {
-      if (action !== "POP") {
-        window.scrollTo(0, 0);
-      }
-      GA.trackPageView({ path: location.pathname });
-    });
-    return () => unlisten();
-  }, [history]);
-
   return (
     <Switch>
       <Route exact path="/" component={MainKo} />
--- webpack.config.js
+++ webpack.config.js
@@ -2,11 +2,7 @@ const path = require("path");
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
-const webpack = require("webpack");
-const dotenv = require("dotenv");
 const tsconfig = require("./tsconfig.json");
-
-dotenv.config();
 
 function convertPathsToAlias() {
   const { baseUrl, paths } = tsconfig.compilerOptions;
@@ -15,7 +11,10 @@ function convertPathsToAlias() {
   for (const pathsKey in paths) {
     const aliasKey = pathsKey.substring(0, pathsKey.length - 2); // remove '/*'
     const pathsValue = paths[pathsKey][0];
-    const resolvedTo = `${baseUrl}/${pathsValue.substring(0, pathsValue.length - 2)}`; // remove '/*'
+    const resolvedTo = `${baseUrl}/${pathsValue.substring(
+      0,
+      pathsValue.length - 2
+    )}`; // remove '/*'
 
     alias[aliasKey] = path.resolve(__dirname, resolvedTo);
   }
@@ -28,20 +27,22 @@ module.exports = (env) => {
   const { mode } = env;
   const isProduction = mode === "production";
 
-  const devServer = !isProduction ? {
-    contentBase: path.resolve("./build"),
-    index: "index.html",
-    port: 3000,
-    open: true,
-    historyApiFallback: true,
-  } : undefined;
+  const devServer = !isProduction
+    ? {
+        contentBase: path.resolve("./build"),
+        index: "index.html",
+        port: 3000,
+        open: true,
+        historyApiFallback: true,
+      }
+    : undefined;
 
   return {
     entry: "./src/index.tsx",
     output: {
       filename: "bundle.js",
       path: path.resolve("./build"),
-      publicPath: "/"
+      publicPath: "/",
     },
     resolve: {
       fallback: {
@@ -63,7 +64,7 @@ module.exports = (env) => {
               loader: "url-loader",
               options: {
                 limit: 10000,
-                name: 'static/[name].[ext]',
+                name: "static/[name].[ext]",
               },
             },
           ],
@@ -82,10 +83,6 @@ module.exports = (env) => {
       new HtmlWebPackPlugin({
         template: "./public/index.html",
         filename: "index.html",
-      }),
-      new webpack.EnvironmentPlugin({
-        NODE_ENV: mode,
-        ...process.env,
       }),
       new CleanWebpackPlugin(),
       new CopyWebpackPlugin({
