4942d43db614647182c0b1c42395032527e80a4b
HoseungJang <hsjang.dev@gmail.com>
2023-11-20 04:18:00 +0900

    scroll to top when push or replace

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { Switch, Route } from "react-router-dom";
+import React, { useEffect } from "react";
+import { Switch, Route, useHistory } from "react-router-dom";
 
 import { MainKo } from "./Main/Ko";
 import { MainEn } from "./Main/En";
@@ -7,6 +7,16 @@ import { ExperienceKo } from "./Experience/Ko";
 import { ExperienceEn } from "./Experience/En";
 
 export function RouteSwitch() {
+  const history = useHistory();
+
+  useEffect(() => {
+    history.listen((_, action) => {
+      if (action !== "POP") {
+        window.scrollTo({ top: 0 });
+      }
+    });
+  }, [history]);
+
   return (
     <Switch>
       <Route exact path="/" component={MainKo} />
