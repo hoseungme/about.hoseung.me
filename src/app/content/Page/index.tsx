88213ee4863663b75ec3d7702026cdce8c69cba4
hoseungme <hsjang.dev@gmail.com>
2024-11-24 21:58:17 +0900

    move content

--- src/app/content/Page/index.tsx
+++ src/app/content/Page/index.tsx
@@ -1,12 +1,8 @@
 import "./style.scss";
 
 import { Markdown } from "@/components/Markdown";
-import { readFileSync } from "fs";
-import { join } from "path";
 
-export function Page() {
-  const content = readFileSync(join(process.cwd(), "src/app/content/Page/content.md")).toString();
-
+export function Page({ content }: { content: string }) {
   return (
     <div className="pt-4">
       <Markdown>{content}</Markdown>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
 import { Metadata } from "next";
+import { readFileSync } from "fs";
+import { join } from "path";
 import { Page } from "./content/Page";
 
 export default function Home() {
-  return <Page />;
+  const content = readFileSync(join(process.cwd(), "src/app/data/index.md")).toString();
+  return <Page content={content} />;
 }
 
 const title = "장호승 이력서";
