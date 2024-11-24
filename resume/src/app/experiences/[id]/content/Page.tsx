812f25a0fc9af2aebbc8489fc58106c6d5231cd1
hoseungme <hsjang.dev@gmail.com>
2024-11-24 21:28:47 +0900

    move content

--- resume/src/app/experiences/[id]/content/Page.tsx
+++ resume/src/app/experiences/[id]/content/Page.tsx
@@ -3,7 +3,7 @@ import { readFileSync } from "fs";
 import { join } from "path";
 
 export function Page({ id }: { id: string }) {
-  const content = readFileSync(join(process.cwd(), `src/app/experiences/[id]/content/data/${id}.md`)).toString();
+  const content = readFileSync(join(process.cwd(), `src/app/experiences/[id]/data/${id}.md`)).toString();
 
   return <Markdown>{content}</Markdown>;
 }
