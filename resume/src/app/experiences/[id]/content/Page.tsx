2cbbfefd8911fc32cd0ff68ff2c9b8ed051b257e
hoseungme <hsjang.dev@gmail.com>
2024-11-24 21:30:03 +0900

    handle error

--- resume/src/app/experiences/[id]/content/Page.tsx
+++ resume/src/app/experiences/[id]/content/Page.tsx
@@ -1,9 +1,13 @@
 import { Markdown } from "@/components/Markdown";
 import { readFileSync } from "fs";
+import { permanentRedirect } from "next/navigation";
 import { join } from "path";
 
 export function Page({ id }: { id: string }) {
-  const content = readFileSync(join(process.cwd(), `src/app/experiences/[id]/data/${id}.md`)).toString();
-
-  return <Markdown>{content}</Markdown>;
+  try {
+    const content = readFileSync(join(process.cwd(), `src/app/experiences/[id]/data/${id}.md`)).toString();
+    return <Markdown>{content}</Markdown>;
+  } catch {
+    permanentRedirect("/");
+  }
 }
