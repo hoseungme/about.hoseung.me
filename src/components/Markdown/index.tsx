cc9b7ccb48d53cb98ca627fdcb2b7de821418af9
hoseungme <hsjang.dev@gmail.com>
2025-05-25 21:24:26 +0900

    allow all urls

--- src/components/Markdown/index.tsx
+++ src/components/Markdown/index.tsx
@@ -6,7 +6,7 @@ import rehypeRaw from "rehype-raw";
 
 export function Markdown({ children }: { children: string }) {
   return (
-    <ReactMarkdown className="markdown-body" rehypePlugins={[rehypeRaw]}>
+    <ReactMarkdown className="markdown-body" urlTransform={(url) => url} rehypePlugins={[rehypeRaw]}>
       {children}
     </ReactMarkdown>
   );
