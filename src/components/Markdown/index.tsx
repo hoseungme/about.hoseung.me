314ae698b484b41b93eea6f021f52ed42b621b07
hoseungme <hsjang.dev@gmail.com>
2025-06-04 21:11:52 +0900

    add heading renderer

--- src/components/Markdown/index.tsx
+++ src/components/Markdown/index.tsx
@@ -1,13 +1,19 @@
 import "github-markdown-css/github-markdown-light.css";
 import "./style.scss";
 
-import ReactMarkdown from "react-markdown";
+import ReactMarkdown, { Components } from "react-markdown";
 import rehypeRaw from "rehype-raw";
+import { createElement } from "react";
 
 export function Markdown({ children }: { children: string }) {
   return (
-    <ReactMarkdown className="markdown-body" urlTransform={(url) => url} rehypePlugins={[rehypeRaw]}>
+    <ReactMarkdown className="markdown-body" urlTransform={(url) => url} components={headingComponents} rehypePlugins={[rehypeRaw]}>
       {children}
     </ReactMarkdown>
   );
 }
+
+const headingComponents = (["h1", "h2", "h3", "h4", "h5", "h6"] as const).reduce((components, name) => {
+  components[name] = (props) => createElement(name, { ...props, node: undefined, id: (props.children?.toString() ?? "").toLowerCase().replace(/[^a-z가-힣\d\s]/g, "").replace(/\s/g, "-") });
+  return components;
+}, {} as Components);
